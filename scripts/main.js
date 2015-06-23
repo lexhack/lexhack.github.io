3119bf943dbad44805c892c0a05fa66176abbe84
Clive Chan <doobahead@gmail.com>
2015-06-23 13:56:40 -0400

    Added tagline fading

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,44 @@
 var taglines = [
-	"Hack while listening to your favorite Track.",
-	"Hack then have a Snack.",
-	"Hack while sitting on a beanbag Sack.",
-	"Hack but don't Slack.",
+	"Hack 'n' [Snack.]",
+	"Hack while listening to your favorite [Track.]",
+	"Hack while sitting on a beanbag [Sack.]",
+	"Hack but don't [Slack.]",
+	"Hack up some Green[backs.]",
+	"Hack on PC or [Mac]",
 ];
 
-console.log('exists');
+
+//http://youmightnotneedjquery.com/#fade_in
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 600;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  };
+
+  tick();
+}
+
+
+prevTaglineIndex = -1;
+function newTagline(){
+	do{var newTaglineIndex = Math.floor(Math.random() * taglines.length);}while(newTaglineIndex == prevTaglineIndex);
+	var tagline = taglines[prevTaglineIndex = newTaglineIndex];
+	var formatted = tagline.replace("[","<span class='yellow'>").replace("]","</span>");
+	document.getElementsByClassName("tagline")[0].innerHTML = formatted;
+}
+
 
 window.onload = function() {
-	console.log("loaded");
-	var tagline = taglines[Math.floor(Math.random() * taglines.length)];
-	var words = tagline.split(" ");
-	document.getElementById("tagline-first").innerHTML = words.slice(0, words.length-1).join(" ")+ " ";
-	document.getElementById("tagline-second").innerHTML = words[words.length-1];
+	newTagline();
+	setInterval(function(){
+		fadeIn(document.getElementsByClassName("tagline")[0]);
+		newTagline();
+	},10000);
 }
